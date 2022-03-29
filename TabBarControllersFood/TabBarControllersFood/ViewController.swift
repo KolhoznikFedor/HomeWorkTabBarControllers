3fdbf62ef70fb2dc2acd7cb97f093d2268cc5df4
KolhoznikFedor <megadyper@gmail.com>
2022-03-29 12:41:49 +0300

    project created

---
+++ TabBarControllersFood/TabBarControllersFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarControllersFood
+//
+//  Created by admin on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
